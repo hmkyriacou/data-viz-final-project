cf66b1781d45bc3c8796ca1908a53bc6f72ba707
hmkyriacou <hmkyriacou14@gmail.com>
2023-04-25 23:29:22 -0400

    Color coded counties according to K CLass

--- map.js
+++ map.js
@@ -1,4 +1,11 @@
-import { geoAlbersUsa, geoPath, schemeRdBu, scaleSequential } from "d3";
+import {
+  geoAlbersUsa,
+  geoPath,
+  interpolateTurbo,
+  scaleOrdinal,
+  schemeGnBu,
+  schemeOrRd,
+} from "d3";
 
 import tooltip from "./tooltips";
 
@@ -13,9 +20,52 @@ export default function map(
   const projection = geoAlbersUsa().fitSize([width, height], data);
   const path = geoPath(projection);
 
-  const colorScale = scaleSequential(schemeRdBu).domain([0, 30]);
+  const climate_zone_list = Object.values(
+    climate_zones.reduce((acc, obj, index) => {
+      acc[`${obj.CLS}`] = acc[`${obj.CLS}`] || [];
+      acc[`${obj.CLS}`].push(index);
+      return acc;
+    }, [])
+  ).map((o) => {
+    return climate_zones[o[0]].CLS;
+  });
 
-  console.log(colorScale(15));
+  const fipsToCls = new Map();
+  for (const d of data.features) {
+    fipsToCls.set(
+      d.id,
+      climate_zones.find((o) => {
+        return o.FIPS === d.id;
+      })?.CLS
+    );
+  }
+
+  const colorScale = scaleOrdinal([
+    "rgb(31, 119, 180)",
+    "rgb(174, 199, 232)",
+    "rgb(255, 127, 14)",
+    "rgb(255, 187, 120)",
+    "rgb(44, 160, 44)",
+    "rgb(152, 223, 138)",
+    "rgb(214, 39, 40)",
+    "rgb(255, 152, 150)",
+    "rgb(148, 103, 189)",
+    "rgb(197, 176, 213)",
+    "rgb(140, 86, 75)",
+    "rgb(196, 156, 148)",
+    "rgb(227, 119, 194)",
+    "rgb(247, 182, 210)",
+    "rgb(127, 127, 127)",
+    "rgb(199, 199, 199)",
+    "rgb(188, 189, 34)",
+    "rgb(219, 219, 141)",
+    "rgb(23, 190, 207)",
+    "rgb(158, 218, 229)",
+  ]).domain(climate_zone_list);
+
+  // console.log(colorScale("Cfa"));
+
+  // console.log(fipsToCls);
 
   selection
     .selectAll("path.country")
@@ -23,8 +73,10 @@ export default function map(
     .join("path")
     .attr("d", path)
     .attr("class", "country")
-    .attr("fill", "none")
-    .attr("stroke", "black")
+    .attr("fill", (d) =>
+      fipsToCls.get(d.id) ? colorScale(fipsToCls.get(d.id)) : "black"
+    )
+    .attr("stroke", (d) => colorScale(fipsToCls.get(d.id)))
     .attr("stroke-width", 0.5);
 
   const imgs = selection
@@ -34,13 +86,13 @@ export default function map(
     .style("opacity", (d) => (state.highlighted.includes(d.team) ? 1 : 0.5))
     .attr("x", (d) => projection([d.location.lng, d.location.lat])[0])
     .attr("y", (d) => projection([d.location.lng, d.location.lat])[1])
-    .attr("width", (d) => (state.highlighted.includes(d.team) ? 40 : 20))
-    .attr("height", (d) => (state.highlighted.includes(d.team) ? 40 : 20))
+    .attr("width", (d) => (state.highlighted.includes(d.team) ? 50 : 30))
+    .attr("height", (d) => (state.highlighted.includes(d.team) ? 50 : 30))
     .attr("xling:href", (d) => d.img)
     .style("transform", (d) =>
       state.highlighted.includes(d.team)
-        ? "translate(-20px, -20px)"
-        : "translate(-10px, -10px)"
+        ? "translate(-25px, -25px)"
+        : "translate(-15px, -15px)"
     );
 
   imgs.on("click", (e, d) => {
